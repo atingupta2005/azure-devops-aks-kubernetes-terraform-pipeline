87a54fdd8f0c032f923e3ecbb55e6e041e6b5419
Atin Gupta <atingupta2005@gmail.com>
2021-10-16 06:28:33 +0530

    -

---
+++ README.md.sh
@@ -0,0 +1,4 @@
+# Provision Azure AKS Cluster using Terraform and Azure DevOps
+
+## For Step by Step Instructions
+- [Step by Step Instructions](https://github.com/atingupta2005/azure-aks-kubernetes-masterclass/tree/master/25-Azure-DevOps-Terraform-Azure-AKS)
